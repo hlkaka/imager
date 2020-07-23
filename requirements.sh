27eadb67ac461c5e662c532f2646947a18cd8a67
Hussam Kaka <hussamlkaka@gmail.com>
2020-07-23 10:34:59 -0400

    added requirements script

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+apt update
+apt install wget htop vim screen python3-pip
+
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+
+mkdir env
+python3 -m venv env
+
+source env/bin/activate
+
+pip3 install -U tqdm segmentation-models-pytorch pytorch-lightning albumentations
