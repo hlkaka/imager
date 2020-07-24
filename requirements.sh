7ade47cc7615f6a67d8d8edd43eac5f7306cf244
Hussam Kaka <hussamlkaka@gmail.com>
2020-07-24 09:36:35 -0400

    changed pip3 to pip

--- requirements.sh
+++ requirements.sh
@@ -3,15 +3,15 @@ apt upgrade
 
 apt install wget htop vim screen python3-pip unzip libglib2.0-0 jupyterlab
 
-pip3 install --upgrade pip
-pip3 install virtualenv
+pip install --upgrade pip
+pip install virtualenv
 
 mkdir env
-python3 -m venv env
+python -m venv env
 
 source env/bin/activate
 
 # nightly pytorch
-#pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
+pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
 
-pip3 install -U tqdm segmentation-models-pytorch pytorch-lightning albumentations SimpleITK
+pip install -U tqdm segmentation-models-pytorch pytorch-lightning albumentations SimpleITK
