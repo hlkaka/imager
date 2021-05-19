3caace4eb5c6a3b46771751ded58797f6c7186d0
Hussam Kaka <hussamlkaka@gmail.com>
2021-05-18 23:21:31 -0400

    moved all validation methods from UNet_no_val to UNet

--- models/UNet_L.py
+++ models/UNet_L.py
@@ -53,6 +53,52 @@ class UNet_no_val(pl.LightningModule):
         self.log('train_loss', loss, on_step=True, on_epoch=True, logger=True)
         return loss #, 'log': tensorboard_logs}
 
+    def train_dataloader(self):
+        return DataLoader(self.datasets['train'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(params = self.parameters(), lr=self.lr)
+
+        return [optimizer]
+
+
+class UNet(UNet_no_val):
+    '''
+    Same as UNet but adds validation and testing dataloaders
+    '''
+
+    def __init__(self, datasets, backbone :str = 'resnet34', encoder_weights :str = 'imagenet',
+                 classes :int = 2, activation :str = 'softmax', batch_size :int = 32,
+                 lr = 0.0001, dl_workers = 8, optimizer_params = None, in_channels=3,
+                 loss = 'dice'):
+        super().__init__(datasets, backbone = backbone, encoder_weights=encoder_weights, classes=classes,
+                activation=activation, batch_size=batch_size, lr=lr, dl_workers=dl_workers,
+                optimizer_params=optimizer_params, in_channels=in_channels, loss=loss)
+        
+    def val_dataloader(self):
+        return DataLoader(self.datasets['val'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.datasets['test'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=False)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(params = self.parameters(), lr=self.lr)
+        if self.optimizer_params is None:
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+        else:
+            factor = self.optimizer_params['factor']
+            patience = self.optimizer_params['patience']
+            cooldown = self.optimizer_params['cooldown']
+            min_lr = self.optimizer_params['min_lr']
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=factor, patience=patience, cooldown=cooldown, min_lr=min_lr)
+
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': scheduler,
+            'monitor': 'val_loss_mean'
+        }
+        #[optimizer], [scheduler]
+
     def validation_step(self, batch, batch_nb):
         images, masks, _, _ = batch
 
@@ -94,44 +140,3 @@ class UNet_no_val(pl.LightningModule):
         self.log('test_loss_mean', test_loss_mean, logger=True)
 
         return {'test_loss': test_loss_mean}
-
-    def train_dataloader(self):
-        return DataLoader(self.datasets['train'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=True)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(params = self.parameters(), lr=self.lr)
-        if self.optimizer_params is None:
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
-        else:
-            factor = self.optimizer_params['factor']
-            patience = self.optimizer_params['patience']
-            cooldown = self.optimizer_params['cooldown']
-            min_lr = self.optimizer_params['min_lr']
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=factor, patience=patience, cooldown=cooldown, min_lr=min_lr)
-
-        return {
-            'optimizer': optimizer,
-            'lr_scheduler': scheduler,
-            'monitor': 'val_loss_mean'
-        }
-        #[optimizer], [scheduler]
-
-
-class UNet(UNet_no_val):
-    '''
-    Same as UNet but adds validation and testing dataloaders
-    '''
-
-    def __init__(self, datasets, backbone :str = 'resnet34', encoder_weights :str = 'imagenet',
-                 classes :int = 2, activation :str = 'softmax', batch_size :int = 32,
-                 lr = 0.0001, dl_workers = 8, optimizer_params = None, in_channels=3,
-                 loss = 'dice'):
-        super().__init__(datasets, backbone = backbone, encoder_weights=encoder_weights, classes=classes,
-                activation=activation, batch_size=batch_size, lr=lr, dl_workers=dl_workers,
-                optimizer_params=optimizer_params, in_channels=in_channels, loss=loss)
-        
-    def val_dataloader(self):
-        return DataLoader(self.datasets['val'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=False)
-
-    def test_dataloader(self):
-        return DataLoader(self.datasets['test'], persistent_workers=True, batch_size=self.batch_size, num_workers = self.dl_workers, shuffle=False)
