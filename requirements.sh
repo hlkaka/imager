e9868c669ca73d8ad8060af9cc90ded8c556689d
Hussam Kaka <hussamlkaka@gmail.com>
2024-11-17 15:44:18 -0800

    minor changes to requirements including specifying version of pytorch_lightning

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,9 @@
-apt update
-apt -y upgrade
+#apt update
+#apt -y upgrade
 
-apt -y install wget htop vim unzip libglib2.0-0 libsm6 libxext6 libxrender-dev
+#apt -y install wget htop vim unzip libglib2.0-0 libsm6 libxext6 libxrender-dev
 
-sudo apt install python3-pip
+#sudo apt install python3-pip
 pip3 install --upgrade pip
 pip3 install virtualenv
 
@@ -15,11 +15,9 @@ source env/bin/activate
 pip3 install --upgrade pip
 
 # 1.6 CUDA 10.2+
-pip3 install torch torchvision
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-pip3 install -U tqdm tensorboard segmentation-models-pytorch pytorch-lightning albumentations SimpleITK jupyterlab torchsummary unidecode torchsort
-
-pip3 install -U --no-deps Kornia
+pip3 install -U tqdm tensorboard segmentation-models-pytorch pytorch-lightning==1.8.4 albumentations SimpleITK jupyterlab torchsummary unidecode torchsort segmentation-models Kornia
 
 #wget https://hnscc-ds.nyc3.cdn.digitaloceanspaces.com/organized_dataset_2.zip
 
