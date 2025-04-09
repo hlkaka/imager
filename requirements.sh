24d5739e78026b2c01154ceb0df7f9781f3e80bc
Hussam Kaka <hussamlkaka@gmail.com>
2025-04-09 16:40:54 -0700

    updated requirements to avoid creating an environment and to fit with new VastAI templates including avoiding redownloading of PyTorch

--- requirements.sh
+++ requirements.sh
@@ -7,20 +7,20 @@
 pip3 install --upgrade pip
 pip3 install virtualenv
 
-mkdir env
-python -m venv env
+#mkdir env
+#python -m venv env
 
-source env/bin/activate
+#source env/bin/activate
 
 pip3 install --upgrade pip
 
 # 1.6 CUDA 10.2+
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 pip3 install -U tqdm tensorboard segmentation-models-pytorch pytorch-lightning==1.8.4 albumentations SimpleITK jupyterlab torchsummary unidecode torchsort segmentation-models Kornia
 
-#wget https://hnscc-ds.nyc3.cdn.digitaloceanspaces.com/organized_dataset_2.zip
+wget https://hnscc-ds.nyc3.cdn.digitaloceanspaces.com/organized_dataset_2.zip
 
-#echo "Unzipping downloaded dataset"
+echo "Unzipping downloaded dataset"
 
-#unzip -q organized_dataset_2.zip
+unzip -q organized_dataset_2.zip
